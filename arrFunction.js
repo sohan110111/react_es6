9f6f973467d9b8f52ba73ae9f66cbe42bb9537bd
sohan110111 <sohan110111@gmail.com>
2023-07-10 18:23:24 +0600

    arrow function

---
+++ arrFunction.js
@@ -0,0 +1,27 @@
+hello = () => {
+    return "Hello World!";
+}
+
+console.log(hello());
+
+// 
+fun2 = () => {  
+    return "Hello World! 2"; 
+}
+
+console.log(fun2());
+
+// pass in parameters
+fun3 = (x, y) => {
+    return x * y;
+}
+console.log(fun3(2, 3));
+
+// pass in a single parameter
+fun4 = x => {
+    return x * x;
+}
+console.log(fun4(2));
+
+
+

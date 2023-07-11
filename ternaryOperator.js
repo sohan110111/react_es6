2ea564772c811c246b88ca7305ee6da37a14ab4c
sohan110111 <sohan110111@gmail.com>
2023-07-11 11:00:02 +0600

    ternary Operator

---
+++ ternaryOperator.js
@@ -0,0 +1,6 @@
+console.log(1?"Condition True":"Condition False");
+// arrow function 
+const arrowFunction = (a,b) => a+b;
+const arrowFunction2 = (a,b) => a-b;
+//print - value function
+console.log(false?arrowFunction(1,2):arrowFunction2(1,2));
